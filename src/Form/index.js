01d24a0ddb58c416790d567056803ed7bc3c869f
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-06-21 12:27:01 +0200

    Form style added

---
+++ src/Form/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import "../currencies";
+import "./result";
+import { useState } from "react";
+
+export const Form = ({ calculateResult, result }) => {
+    const [currency, setCurrency] = useState()
+}
