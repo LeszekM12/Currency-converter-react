eba1ea3ef900e53ab818cff22ce3ee003e0599e4
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-06-21 12:13:56 +0200

    currencies added

---
+++ src/currencies.js
@@ -0,0 +1,18 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.3307,
+    },
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.0500,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.1254,
+
+    },
+];
