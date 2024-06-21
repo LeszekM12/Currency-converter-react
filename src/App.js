6abbc349fdda973ce46838c507ed30119355b4c2
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-06-21 12:20:42 +0200

    calculate result interaction

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import './App.css';
+import { useState } from 'react';
+import './Container';
+import { currencies } from './currencies';
+import { Container } from './Container';
 
 function App() {
+
+  const [result, setResult] = useState();
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+      .find(({ short }) => short === currency)
+      .rate;
+
+      setResult({
+        sourceAmount: +amount,
+        targetAmount: amount / rate,
+        currency,
+      });
+  }
   return (
     <div>elo</div>
   );
