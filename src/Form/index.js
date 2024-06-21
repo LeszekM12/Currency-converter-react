c011ed86d8c25c945699b8f2ee2475c7ef4810db
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-06-21 13:10:10 +0200

    onSubmit return fixed

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,34 +2,35 @@ import "./style.css";
 import { useState } from "react";
 
 export const Form = ({ calculateResult, result }) => {
-    const [currency, setCurrency] = useState()
+    const [currency, setCurrency] = useState();
     const [amount, setAmount] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(currency, amount);
+    };
+
+    return (
+        <form className="form" onSubmit={onSubmit}>
+            <h1 className="form_header">
+                Kalkulator walut
+            </h1>
+            <p>
+                <label>
+                    <span className="form_labelText">
+                        Kwota PLN:
+                    </span>
+                    <input
+                    value={amount}
+                    className="form__field"
+                    type="number"
+                    min="0.01" 
+                    step="any" 
+                    required
+                    />
+                </label>
+            </p>
+        </form>
+    )
 }
 
-const onSubmit = (event) => {
-    event.preventDefault();
-    calculateResult(currency, amount);
-};
-
-return (
-    <form className="form" onSubmit={onSubmit}>
-        <h1 className=" form_header">
-            Kalkulator walut
-        </h1>
-        <p>
-            <label>
-                <span className="form_labelText">
-                    Kwota PLN:
-                </span>
-                <input
-                value={amount}
-                className="form__field"
-                type="number"
-                min="0.01" 
-                step="any" 
-                required
-                />
-            </label>
-        </p>
-    </form>
-)
