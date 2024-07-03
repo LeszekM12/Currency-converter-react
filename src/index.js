f6c3ba18e1c751dbbbdfeee094ff7e85e2202c45
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-03 14:34:26 +0200

    Global theme Style

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { theme } from './theme';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+    color: {
+        teal: "rgb(0, 128, 128)"
+    },
+    breakpoints: {
+        mobileMax: 767,
+    },
+};
