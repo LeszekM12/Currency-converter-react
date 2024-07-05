c45e1632a324c87f1ecbe836d0e7d030e08028fa
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-05 11:53:40 +0200

    calculate result moved to form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import { useState } from 'react';
-import { currencies } from './currencies';
 import { Container } from './Container';
 import { Form } from './Form';
 import { Clock } from './Clock';
 
 function App() {
 
-  const [result, setResult] = useState(null);
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies
-      .find(({ short }) => short === currency)
-      .rate;
-
-      setResult({
-        sourceAmount: +amount,
-        targetAmount: amount / rate,
-        currency,
-      });
-  }
   return (
     <Container>
       <Clock/>
-        <Form
-          result={result}
-          calculateResult={calculateResult}
-        />
+        <Form/>
     </Container>
   );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -3,7 +3,20 @@ import { useState } from "react";
 import { currencies } from "../currencies";
 import { Result } from "./Result";
 
-export const Form = ({ calculateResult, result }) => {
+export const Form = () => {
+  const [result, setResult] = useState(null);
+
+  const calculateResult = (currency, amount) => {
+    const rate = currencies
+      .find(({ short }) => short === currency)
+      .rate;
+
+      setResult({
+        sourceAmount: +amount,
+        targetAmount: amount / rate,
+        currency,
+      });
+  }
   const [currency, setCurrency] = useState(currencies[0].short);
   const [amount, setAmount] = useState("");
 
