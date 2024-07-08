de2da98baf956ab2abcdd55db0c168c3a2759130
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-08 19:50:05 +0200

    status data

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,7 +1,14 @@
-import { StyledForm, Header, Field, Label, Button, ExchangeDate} from "./styled";
+import {
+  StyledForm,
+  Header,
+  Field,
+  Label,
+  Button,
+  ExchangeDate,
+} from "./styled";
 import { useState } from "react";
 import { Result } from "./Result";
-import { useJsonData } from "../useJsonData"; 
+import { useJsonData } from "../useJsonData";
 
 export const Form = () => {
   const [result, setResult] = useState(null);
@@ -10,71 +17,71 @@ export const Form = () => {
   const calculateResult = (currency, amount) => {
     const rate = ratesData.data[currency].value;
 
-      setResult({
-        sourceAmount: +amount,
-        targetAmount: amount / rate,
-        currency,
-      });
-  }
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
   const [currency, setCurrency] = useState("EUR");
   const [amount, setAmount] = useState("");
 
   const onSubmit = (event) => {
     event.preventDefault();
     calculateResult(currency, amount);
-  }
+  };
 
   const getExchangeDate = (ratesData) => {
-      return new Date(ratesData.date).toLocaleDateString(undefined);
+    return new Date(ratesData.date).toLocaleDateString(undefined);
   };
 
   return (
     <StyledForm onSubmit={onSubmit}>
       <Header>Kalkulator walut</Header>
-      <p>
-        <label>
-          <Label>Kwota PLN:</Label>
-          <Field
-            value={amount}
-            onChange={({ target }) => setAmount(target.value)}
-            placeholder="Wpisz kwotę PLN"
-            type="number"
-            min="0.01"
-            step="any"
-            required
-          />
-        </label>
-      </p>
-      <p>
-        <label>
-          <Label>Waluta:</Label>
-          <Field
-            as="select"
-            value={currency}
-            onChange={({ target }) => setCurrency(target.value)}
-          >
-            {Object.keys(ratesData.data || {}).map((currency) => (
-              <option 
-                key={currency} 
+      {ratesData.status === "loading" ? 
+        ratesData.status === "error" : (
+        <>
+          <p>
+            <label>
+              <Label>Kwota PLN:</Label>
+              <Field
+                value={amount}
+                onChange={({ target }) => setAmount(target.value)}
+                placeholder="Wpisz kwotę PLN"
+                type="number"
+                min="0.01"
+                step="any"
+                required
+              />
+            </label>
+          </p>
+          <p>
+            <label>
+              <Label>Waluta:</Label>
+              <Field
+                as="select"
                 value={currency}
+                onChange={({ target }) => setCurrency(target.value)}
               >
-                {currency}
-              </option>
-            ))}
-          </Field>
-        </label>
-      </p>
-      <p>
-        <Button>Przelicz!</Button>
-      </p>
-      <ExchangeDate>
-      Kursy walut pobierane są z Europejskich Instytucji Finansowych.
-      <br />
-      <strong>
-        Aktualne na dzień: {getExchangeDate(ratesData)}
-      </strong>
-      </ExchangeDate>
-      <Result result={result}/>
+                {Object.keys(ratesData.data || {}).map((currency) => (
+                  <option key={currency} value={currency}>
+                    {currency}
+                  </option>
+                ))}
+              </Field>
+            </label>
+          </p>
+          <p>
+            <Button>Przelicz!</Button>
+          </p>
+          <ExchangeDate>
+            Kursy walut pobierane są z Europejskich Instytucji Finansowych.
+            <br />
+            <strong>Aktualne na dzień: {getExchangeDate(ratesData)}</strong>
+          </ExchangeDate>
+          <Result result={result} />
+        </>
+      )}
     </StyledForm>
   );
 };
