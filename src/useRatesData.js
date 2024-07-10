b14789771e39d258b588b19ade273c1eab641f2b
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-07-10 13:06:56 +0200

    api moved to constant

--- src/useRatesData.js
+++ src/useRatesData.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+export const getApiData = async () => {
+    const response = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_ipczFcXW1F0EkRVxiXkN1rNTrts8OwZXz73t0MQx&currencies=&base_currency=PLN");
+    return response;
+};
+
 export const useRatesData = () => {
    const [ ratesData, setRatesData ] = useState({status: "loading",});
 
     useEffect(() => {
         const axiosRates = async () => {
             try {
-                const response = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_ipczFcXW1F0EkRVxiXkN1rNTrts8OwZXz73t0MQx");
+                const response = await getApiData();
                 setRatesData({ 
                     data: response.data.data,
                     date: response.data.meta.last_updated_at,
